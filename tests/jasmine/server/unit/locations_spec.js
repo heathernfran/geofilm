7b1f32694f048c8a8a4572dd82df3413d993744d
Heather Frantisak <hfrantisak@mvnp.com>
2015-07-31 09:41:33 -1000

    Add/modify test functions. Geolocation is not being read.

--- tests/jasmine/server/unit/locations_spec.js
+++ tests/jasmine/server/unit/locations_spec.js
@@ -2,12 +2,44 @@ describe('Places', function() {
 
   beforeEach(function() {
 
+    // add new location to db
+    function addNewLocation(placename, clientname, notes) {
+      Places.insert({
+        placename: placename,
+        clientname: clientname,
+        notes: notes
+      }, setInterval());
+    }
+
+    // get current coordinates
+    function setInterval() {
+      Tracker.autorun(function() {
+        Geolocation.currentLocation(function(position) {
+          Session.set('lat', position.coords.latitude);
+          Session.set('long', position.coords.longitude);
+          Session.set('timestamp', position.timestamp);
+          // Places.insert({
+          //   latitude: Session.get('lat'),
+          //   longitude: Session.get('long'),
+          //   timestamp: Session.get('timestamp')
+          // });
+        }, 10000);
+      });
+    }
+
+
     this.placename = 'test place name';
     this.clientname = 'test client name';
     this.notes = 'test notes for this doc';
     this.place = new Place(this.placename, this.clientname, this.notes);
 
+    spyOn(this.place, 'addNewLocation');
+
     addNewLocation(this.placename, this.clientname, this.notes);
+
+    // spyOn(this.place, 'addNewLocation');
+
+    // setInterval();
 
   });
 
@@ -15,9 +47,17 @@ describe('Places', function() {
     expect(this.place.placename).toEqual(this.placename);
     expect(this.place.clientname).toEqual(this.clientname);
     expect(this.place.notes).toEqual(this.notes);
-    expect(this.place.latitude).not.toBe(null);
-    expect(this.place.longitude).not.toBe(null);
-    expect(this.place.timestamp).not.toBe(null);
+
+    expect(Session.get('lat')).not.toBeUndefined();
+
+    // expect(this.place.latitude).not.toBeUndefined();
+    // expect(this.place.longitude).not.toBeUndefined();
+    // expect(this.place.timestamp).not.toBeUndefined();
+
+
+    // honolulu
+    // expect(this.place.latitude).toEqual(21.3077169);
+    // expect(this.place.longitude).toEqual(-157.8614089);
   });
 
   describe('adds and saves new object', function() {
