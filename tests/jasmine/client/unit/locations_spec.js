201f724f31d14fc9cb39465ac4bdab123cff60fc
Heather Frantisak <hfrantisak@mvnp.com>
2015-07-30 14:16:26 -1000

    Add Geolocation for testing current location.

--- tests/jasmine/client/unit/locations_spec.js
+++ tests/jasmine/client/unit/locations_spec.js
@@ -2,6 +2,14 @@ describe('Places', function() {
 
 describe("insert places", function() {
   it("should insert into database", function() {
+
+    Geolocation.currentLocation(function(pos) {
+      var latitude = pos.coords.latitude,
+          longitude = pos.coords.longitude,
+          timestamp = pos.timestamp;
+    });
+
+
     var testPlace = 'test place',
         testClient = 'test client',
         testNotes = 'test notes';
@@ -9,8 +17,8 @@ describe("insert places", function() {
     spoyOn(Places, 'insert').and.returnValue(result);
   });
 
-  expect(Places.insertPlace(testPlace, testClient, testNotes)).toBe(result);
-  expect(Locations.find.calls.argsFor(0)).toEqual(testLoc);
+  expect(Places.insertPlace(testPlace, testClient, testNotes, latitude, longitude, timestamp)).toBe(result);
+  // expect(Locations.find.calls.argsFor(0)).toEqual(result);
 });
 
 });
