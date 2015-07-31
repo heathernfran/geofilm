29ed77b8ce9861ba1785beaeea2ecb44d5449907
Heather Frantisak <hfrantisak@mvnp.com>
2015-07-31 09:42:52 -1000

    Add geolocation functions. Tests are not finishing.

--- tests/jasmine/client/unit/locations_spec.js
+++ tests/jasmine/client/unit/locations_spec.js
@@ -10,15 +10,30 @@ describe("insert places", function() {
     });
 
 
-    var testPlace = 'test place',
-        testClient = 'test client',
-        testNotes = 'test notes';
+    var testPlace = 'test place 0',
+        testClient = 'test client 0',
+        testNotes = 'test notes 0';
 
-    spoyOn(Places, 'insert').and.returnValue(result);
+    spyOn(Places, 'insert').and.returnValue(result);
   });
 
   expect(Places.insertPlace(testPlace, testClient, testNotes, latitude, longitude, timestamp)).toBe(result);
   // expect(Locations.find.calls.argsFor(0)).toEqual(result);
 });
 
+});
+
+
+describe('add new location', function() {
+  it('should add geolocation and user data into database', function() {
+    var testPlace = 'test place 1',
+        testClient = 'test client 1',
+        testNotes = 'test notes 1';
+
+    addNewLocation(testPlace, testClient, testNotes);
+
+    spyOn(Places, 'insert').and.returnValue(result);
+  });
+
+  expect(Places.insertPlace(testPlace, testClient, testNotes, latitude, longitude, timestamp)).toBe(result);
 });
