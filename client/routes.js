bd01d1cd4b540249c564aaca8f085c6f4841a53f
Heather Frantisak <hfrantisak@mvnp.com>
2015-10-05 09:11:12 -1000

    Set current location template in separate file. Add routing and navigation for it.

--- client/routes.js
+++ client/routes.js
@@ -1,23 +1,28 @@
 Router.configure({
   layoutTemplate: 'navs'
-});
+})
 
 Router.route('/', function() {
   this.render('default');
   this.template('default');
-});
+})
 
 Router.route('/login', function() {
   this.render('login');
   this.template('login');
-});
+})
 
 Router.route('/addlocation', function() {
   this.render('geoform');
   this.template('geoform');
-});
+})
 
 Router.route('/showlocations', function() {
   this.render('showall');
   this.template('showall');
-});
+})
+
+Router.route('/currentlocaiton', function() {
+  this.render('currentlocation');
+  this.template('currentlocation');
+})
