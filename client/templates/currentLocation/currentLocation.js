3af808fb365103dc8cf99b1ff1d22eac1866250e
Heather Frantisak <hfrantisak@mvnp.com>
2015-10-07 17:58:14 -1000

    Add leaflet package

--- client/templates/currentLocation/currentLocation.js
+++ client/templates/currentLocation/currentLocation.js
@@ -1,3 +1,12 @@
+// Leaflet package
+// set image path
+L.Icon.Default.imagePath = 'packages/beavanhunt_leaflet/images'
+var map = L.map('map')
+
+// set map tile
+L.tileLayer.provider('Thunderforest.Outdoors').addTo(map)
+
+
 // geolocation api
 var geoOptions = {
   enableHighAccuracy: false,
