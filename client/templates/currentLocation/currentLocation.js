9671ceb94e2123192c1d8ad309aee1cbcef8cc1e
Heather Frantisak <hfrantisak@mvnp.com>
2015-10-08 15:01:49 -1000

    Wrap geolocation function inside Tracker, for reactively getting location.

--- client/templates/currentLocation/currentLocation.js
+++ client/templates/currentLocation/currentLocation.js
@@ -1,41 +1,44 @@
 // Leaflet package
 // set image path
-L.Icon.Default.imagePath = 'packages/beavanhunt_leaflet/images'
-var map = L.map('map')
+// L.Icon.Default.imagePath = 'packages/beavanhunt_leaflet/images'
+// var map = L.map('map')
 
 // set map tile
-L.tileLayer.provider('Thunderforest.Outdoors').addTo(map)
+// L.tileLayer.provider('Thunderforest.Outdoors').addTo(map)
 
 
-// geolocation api
-var geoOptions = {
-  enableHighAccuracy: false,
-  maximumAge: 10000,
-  timeout: 5000
-};
+Tracker.autorun(function() {
 
-// TODO remove .getCurrentPostion and .clearWatch functions, use navigator.geolocation.watchPosition
-var watchId = navigator.geolocation.getCurrentPosition(getLocation, errorForLocation, geoOptions);
+  // geolocation api
+  var geoOptions = {
+    enableHighAccuracy: false,
+    maximumAge: 10000,
+    timeout: 5000
+  };
 
-function getLocation(position) {
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
-  $('#current #lat').append(latitude);
-  $('#current #long').append(longitude);
-  mapLocation(latitude, longitude);
-  clearLocation(watchId);
-}
+  // TODO remove .getCurrentPostion and .clearWatch functions, use navigator.geolocation.watchPosition
+  var watchId = navigator.geolocation.getCurrentPosition(getLocation, errorForLocation, geoOptions);
 
-function mapLocation(posLat, posLong) {
-  var img = new Image();
-  img.src = 'https://maps.googleapis.com/maps/api/staticmap?center=' + posLat + ',' + posLong + '&zoom=13&size=300x300&sensor=false';
-  $('#map').append(img);
-}
+  function getLocation(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    $('#current #lat').append(latitude);
+    $('#current #long').append(longitude);
+    mapLocation(latitude, longitude);
+    clearLocation(watchId);
+  }
 
-function clearLocation(watchId) {
-  navigator.geolocation.clearWatch(watchId);
-}
+  function mapLocation(posLat, posLong) {
+    var img = new Image();
+    img.src = 'https://maps.googleapis.com/maps/api/staticmap?center=' + posLat + ',' + posLong + '&zoom=13&size=300x300&sensor=false';
+    $('#map').append(img);
+  }
 
-function errorForLocation() {
-  alert('Could not get current location');
-}
+  function clearLocation(watchId) {
+    navigator.geolocation.clearWatch(watchId);
+  }
+
+  function errorForLocation() {
+    alert('Could not get current location');
+  }
+})
